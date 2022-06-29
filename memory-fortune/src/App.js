8696514a2d5f2cbfa8803ac0ea5975a4d0ee6bf4
Yasmin <yasmin.bunnie@gmail.com>
2022-06-29 23:59:46 +0100

    deleted necassary files, code and added new base CSS

---
+++ memory-fortune/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Fortune Teller's Memory Game</h1>
+      <button> New Game</button>
+    </div>
+  );
+}
+
+export default App;
